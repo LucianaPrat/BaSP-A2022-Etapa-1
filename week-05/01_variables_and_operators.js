6dfdedbc335568b4af6664082c3b1c4b0c9188ef
Luciana Prates <lucianaprates10@gmail.com>
2022-09-19 17:36:39 -0300

    BaSP-W05-2022: Update css, add first exercise.

---
+++ week-05/01_variables_and_operators.js
@@ -0,0 +1,43 @@
+console.log('------> EXERCISE 1 : VARIABLES AND OPERATORS <------');
+
+// EXERCISE 1.a: 
+/* Crear dos variables numéricas y utilizar el operador suma
+para guardar el valor de la suma de ambos números en una 3er variable.*/
+
+console.log('------ EXERCISE 1.a ------');
+
+var num1 = 10;
+
+var num2 = 20;
+
+var numSum = num1 + num2;
+
+console.log(numSum);
+
+// EXERCISE 1.b: 
+/* Crear dos variables de tipo String y concatenarlas guardando el 
+resultado en una 3er variable. */
+
+console.log('------ EXERCISE 1.b ------');
+
+var wordOne = 'Hi';
+
+var wordTwo = 'world!';
+
+var words = wordOne + ' ' + wordTwo;
+
+console.log(words);
+
+// EXERCISE 1.c: 
+/* Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) 
+guardando el resultado de la suma en una 3er variable (utilizar length). */
+
+console.log('------ EXERCISE 1.c ------');
+
+var stringOne = 'Good ';
+
+var stringTwo = 'Morning';
+
+var stringsLength = (stringOne + stringTwo).length;
+
+console.log(stringsLength);
